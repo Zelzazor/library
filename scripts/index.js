cd768e46114271f1d841aebdcd9858e7de28748f
Felipe Ramírez <zelzazor.ramirez62@gmail.com>
2020-10-21 01:46:10 -0400

    Add: Borrar libros y cambio de estado

--- scripts/index.js
+++ scripts/index.js
@@ -17,6 +17,15 @@ function Book(author, title, numberOfPages, read) {
     this.read = read;
 }
 
+Book.prototype.toggleRead = function () {
+    if (this.read) {
+        this.read = false;
+    }
+    else {
+        this.read = true;
+    }
+}
+
 function addBookToLibrary(author, title, numberOfPages, read) {
     let book = new Book(author, title, parseInt(numberOfPages), read);
     myLibrary.push(book);
@@ -25,31 +34,51 @@ function addBookToLibrary(author, title, numberOfPages, read) {
 
 function refreshTable() {
     centralTableBody.innerHTML = "";
-    for (let i = 0; i < myLibrary.length; i++) {
-        let row = document.createElement("tr");
-        let author = document.createElement("td");
-        let title = document.createElement("td");
-        let numberOfPages = document.createElement("td");
-        let read = document.createElement("td");
-        let markAsRead = document.createElement("td");
-        let remove = document.createElement("td");
-        let markbtn = document.createElement("button");
-        let removebtn = document.createElement("button");
-        removebtn.textContent = "Remove";
-        markbtn.textContent = "Mark as Read";
-        remove.appendChild(removebtn);
-        markAsRead.appendChild(markbtn);
-        author.textContent = myLibrary[i].author;
-        title.textContent = myLibrary[i].title;
-        numberOfPages.textContent = myLibrary[i].numberOfPages;
-        read.textContent = myLibrary[i].read ? "Read" : "Not Read";
-        row.appendChild(author);
-        row.appendChild(title);
-        row.appendChild(numberOfPages);
-        row.appendChild(read);
-        row.appendChild(markAsRead);
-        row.appendChild(remove);
-        centralTableBody.appendChild(row);
+    if (myLibrary.length > 0) {
+        for (let i = 0; i < myLibrary.length; i++) {
+            let row = document.createElement("tr");
+            let author = document.createElement("td");
+            let title = document.createElement("td");
+            let numberOfPages = document.createElement("td");
+            let read = document.createElement("td");
+            let markAsRead = document.createElement("td");
+            let remove = document.createElement("td");
+            let markbtn = document.createElement("button");
+            let removebtn = document.createElement("button");
+            removebtn.textContent = "Remove";
+            removebtn.dataset.row = i;
+            markbtn.dataset.row = i;
+            markbtn.textContent = myLibrary[i].read ? "Mark as Not Read" : "Mark as Read";
+
+
+            markbtn.addEventListener("click", (e) => {
+                let row = parseInt(e.target.dataset.row);
+                myLibrary[row].toggleRead();
+                refreshTable();
+            });
+            removebtn.addEventListener("click", (e) => {
+                let row = parseInt(e.target.dataset.row);
+                myLibrary.splice(row, 1);
+                refreshTable();
+            });
+
+            remove.appendChild(removebtn);
+            markAsRead.appendChild(markbtn);
+            author.textContent = myLibrary[i].author;
+            title.textContent = myLibrary[i].title;
+            numberOfPages.textContent = myLibrary[i].numberOfPages;
+            read.textContent = myLibrary[i].read ? "Read" : "Not Read";
+            row.appendChild(author);
+            row.appendChild(title);
+            row.appendChild(numberOfPages);
+            row.appendChild(read);
+            row.appendChild(markAsRead);
+            row.appendChild(remove);
+            centralTableBody.appendChild(row);
+        }
+    }
+    else {
+        centralTableBody.innerHTML = "<td>N/A</td><td>N/A</td><td>N/A</td><td>N/A</td><td><button>Sin datos</button></td><td><button>Sin datos</button></td>";
     }
 }
 
